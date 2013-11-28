fe6ae5d8fc914739242357a614fc0c1355fab8a9
jjcua <jjgamer371@gmail.com>
2013-11-28 17:50:17 +0800

    Service commit

--- SODAwcfService/SODAwcfService/VideoplayerService.svc.cs
+++ SODAwcfService/SODAwcfService/VideoplayerService.svc.cs
@@ -7,6 +7,8 @@ using System.ServiceModel.Web;
 using System.Text;
 using SODAwcfService.SodaDBDataSetTableAdapters;
 using System.ServiceModel.Activation;
+using SODAwcfService.XMLModels;
+
 namespace SODAwcfService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "VideoplayerService" in code, svc and config file together.
@@ -17,7 +19,8 @@ namespace SODAwcfService
         private PortalDataSetTableAdapters.CustomerTableAdapter customer = new PortalDataSetTableAdapters.CustomerTableAdapter();
         private CategoryTableAdapter category = new CategoryTableAdapter();
         private SpecificTableAdapter spec = new SpecificTableAdapter();
-
+        private TopicsTableAdapter topicAdapter = new TopicsTableAdapter();
+        private chapterTableAdapter chapterAdapter = new chapterTableAdapter();
         public Users validate(string username, string password)
         {
               var result = account.GetDataByUserName(username);
@@ -60,14 +63,31 @@ namespace SODAwcfService
                     id = title.Id,
                     code = title.TitleCode,
                     name = title.Title,
-                    filename = System.IO.Path.GetFileName(title.ImageURL),
+                    filename = title.filename,
+                    
                     summary = title.Overview,
-                    downloadNews = "False",
-                    questionAnswerChangeDate = string.Format("{0:M/d/yyyy}", default(DateTime)),
-                    time = string.Format("{0}", default(TimeSpan)).Replace(":","")
+                    downloadNews = title.isDownloadNews.ToString(),
+                    questionAnswerChangeDate =  string.Format("{0:M/d/yyyy}",title.DateQuestionAnswerChange),
+                    time = "0",
+                    topicList = getTopics(title.Id),
+                    chapterList = getChapters(title.Id)
                 });
             }
             return new trainnowChannels() { titleList=titleList };
+        }
+
+        private List<XMLModels.chapter> getChapters(long specID)
+        {
+            var chapterList = chapterAdapter.GetData().Select(c => new { xmlChapter = new XMLModels.chapter() { name = c.ChapterName, time = c.time.TotalMilliseconds.ToString() }, c.SpecID }).Where(c => c.SpecID == specID);
+
+            return chapterList.Count() > 0 ? chapterList.Select(c => c.xmlChapter).ToList() : null;
+        }
+
+        private List<XMLModels.topic> getTopics(long specID)
+        {
+            //var topics = topicAdapter.GetData().Select(t => new { xmlTopic = new topic() { name = t.Name }, t.SpecId }).Where(t => t.SpecId == specID);
+            var topics = topicAdapter.GetData().Select(t => new { xmlTopic = new XMLModels.topic { name = t.Name }, t.SpecId }).Where(t => t.SpecId == specID);
+            return topics.Count() > 0 ? topics.Select(t => t.xmlTopic).ToList() : null; 
         }
     }
 }
