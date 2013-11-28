7ee7b0229d014438d59a938cae9bb5955ed6b513
jjcua <jjgamer371@gmail.com>
2013-11-28 17:46:41 +0800

    helper commit

--- SODAMvcApplication/SODAPortalMvcApplication/Controllers/EmailHelper.cs
+++ SODAMvcApplication/SODAPortalMvcApplication/Controllers/EmailHelper.cs
@@ -32,7 +32,7 @@ namespace SODAPortalMvcApplication.Controllers
             {
                 SmtpClient smtp_alter = new SmtpClient();
                  
-                throw (exp);
+                //throw (exp);
             }
             finally
             {
