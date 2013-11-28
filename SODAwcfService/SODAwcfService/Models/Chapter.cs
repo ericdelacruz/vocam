25eed1ed9e879a8f5130de8cb464cac8a62de10f
jjcua <jjgamer371@gmail.com>
2013-11-28 17:47:51 +0800

    Model Commits

---
+++ SODAwcfService/SODAwcfService/Models/Chapter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SODAwcfService.Models
+{
+    public class Chapter
+    {
+        public long Id { get; set; }
+        public Nullable<long> SpecID { get; set; }
+        public string ChapterName { get; set; }
+        public Nullable<System.TimeSpan> time { get; set; }
+    }
+}
---
+++ SODAwcfService/SODAwcfService/XMLModel/Channel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+namespace SODAwcfService.XMLModels
+{
+   
+    public class channel
+    {
+        [XmlAttribute]
+        public string name { get; set; }
+        [XmlAttribute]
+        public string image { get; set; }
+        [XmlAttribute]
+        public long id { get; set; }
+        public channel()
+        {
+
+        }
+        public channel(string name,string image, long id)
+        {
+            this.name = name;
+            this.image = image;
+            this.id = id;
+        }
+    }
+}
