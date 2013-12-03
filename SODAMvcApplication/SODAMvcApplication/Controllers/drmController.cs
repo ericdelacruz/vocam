1f7dfd8772f8080ec2a30227474a86c944cee7ca
jjcua <jjgamer371@gmail.com>
2013-12-03 15:57:31 +0800

    Added service references for drm and XML

---
+++ SODAMvcApplication/SODAMvcApplication/Controllers/drmController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SODAMvcApplication.Controllers
+{
+    public class drmController : Controller
+    {
+        //
+        // GET: /drm/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult authenticate(string username,string password, string contentid)
+        {
+            AccountServiceReference.AccountServiceClient accountClient = new AccountServiceReference.AccountServiceClient();
+            PortalServiceReference.PortalServiceClient portalClient = new PortalServiceReference.PortalServiceClient();
+
+            if(accountClient.AuthenticateUser(username,password))
+            {
+                var customer = from c in portalClient.getCustomer()
+                               join a in accountClient.getAccount(username) on c.UserId equals a.Id
+                               select c;
+                if (customer.Count() > 0)
+                {
+                    return View(customer.First());
+                }
+                else
+                    return View();
+                
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
