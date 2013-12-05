9145d99fc8fd387f43bfd9fbefe4f5c65571ae5f
jjcua <jjgamer371@gmail.com>
2013-12-05 14:30:49 +0800

    Controller commit

--- SODAMvcApplication/SODAMvcApplication/Controllers/HomeController.cs
+++ SODAMvcApplication/SODAMvcApplication/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data;
+using Recaptcha.Web;
+using Recaptcha.Web.Mvc;
+using System.Threading.Tasks;
 namespace SODAMvcApplication.Controllers
 {
     public class HomeController : Controller
@@ -21,11 +24,6 @@ namespace SODAMvcApplication.Controllers
         {
 
           
-            //if (cmsServiceClient.Authenticate(password))
-            //{
-            //    //error page
-            //}
-
             
               var  lContentDef = cmsServiceClient.getContent(HOME, string.Empty);
             
@@ -43,12 +41,13 @@ namespace SODAMvcApplication.Controllers
         //GET: /Contact/
         public ActionResult Contact()
         {
-          
+            
             return View();
         }
         [HttpPost]
         public ActionResult contact(CMSServiceReference.Contact contact)
         {
+            contact.isFreePPT = false;
             cmsServiceClient.addContact(contact);
             return RedirectToAction("Index");
         }
@@ -68,6 +67,34 @@ namespace SODAMvcApplication.Controllers
 
 
             return View(lContentDef);
+        }
+        public ActionResult thankyou()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> contactFreeppt(CMSServiceReference.Contact contact)
+        {
+            RecaptchaVerificationHelper recaptchaHelper = this.GetRecaptchaVerificationHelper();
+            if (String.IsNullOrEmpty(recaptchaHelper.Response))
+            {
+                ModelState.AddModelError("", "Captcha answer cannot be empty.");
+                return View(contact);
+            }
+
+            RecaptchaVerificationResult recaptchaResult = await recaptchaHelper.VerifyRecaptchaResponseTaskAsync();
+
+            if (recaptchaResult != RecaptchaVerificationResult.Success)
+            {
+                ModelState.AddModelError("", "Incorrect captcha answer.");
+                return View(contact);
+            }
+           
+            contact.isFreePPT = false;
+            cmsServiceClient.addContact(contact);
+            EmailHelper.SendEmail("test@yahoo.com", contact.Email, "TEST", "TEST BODY");
+            return RedirectToAction("thankyou");
+           
         }
 
     }
