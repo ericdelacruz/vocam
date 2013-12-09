01efb14cd9db709f45b1ead441e64427b696af2d
jjcua <jjgamer371@gmail.com>
2013-12-09 15:57:12 +0800

    added files for xml

---
+++ SODAMvcApplication/SODAMvcApplication/Controllers/xmlController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SODAMvcApplication.Models.XML;
+namespace SODAMvcApplication.Controllers
+{
+    public class xmlController : Controller
+    {
+
+        private PortalServiceReference.PortalServiceClient portalClient = new PortalServiceReference.PortalServiceClient();
+        private CategoriesServiceReference.CatListingServiceClient catClient = new CategoriesServiceReference.CatListingServiceClient();
+        private AccountServiceReference.AccountServiceClient account = new AccountServiceReference.AccountServiceClient();
+        
+        protected override void Dispose(bool disposing)
+        {
+            catClient.Close();
+            account.Close();
+            portalClient.Close();
+            base.Dispose(disposing);
+        }
+        //
+        // GET: /xml/
+        public ActionResult Index()
+        {
+            return View();
+        }
+         
+        public ActionResult validate(string username, string password)
+        {
+            if(!string.IsNullOrEmpty(username.Trim()) && !string.IsNullOrEmpty(password.Trim()) && account.AuthenticateUser(username,password))
+            {
+                var customer = from accnt in account.getAccount(username)
+                               join cust in portalClient.getCustomer() on accnt.Id equals cust.UserId
+                               select cust;
+                
+
+                if(customer.Count() > 0)
+                {
+                    int daysleft = 0;
+                    if (customer.First().DateSubscriptionEnd.HasValue)
+                        daysleft = ((TimeSpan)(DateTime.Now - customer.First().DateSubscriptionEnd.Value)).Days;
+                    return View(new Users() { authorized = true,daysleft=daysleft, shownews = true });
+                }
+                else
+                {
+                    return View(new Users() { authorized = false, daysleft = 0, shownews = true });
+                }
+            }
+            else
+            {
+                return View(new Users() { authorized = false, daysleft = 0, shownews = true });
+            }
+        }
+        public ActionResult channels()
+        {
+            var channels = catClient.get_Categories();
+            return View(channels);
+        }
+        public ActionResult titles()
+        {
+            var titles = catClient.get();
+            
+            return View(titles);
+        }
+    }
+}
---
+++ SODAMvcApplication/SODAMvcApplication/Models/XMLModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SODAMvcApplication.Models.XML
+{
+    public class Users
+    {
+        public bool authorized { get; set; }
+        public int daysleft { get; set; }
+        public bool shownews { get; set; }
+    }
+   
+}
