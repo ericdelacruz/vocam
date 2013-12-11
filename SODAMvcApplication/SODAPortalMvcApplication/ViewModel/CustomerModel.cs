7cde0ef4ed3913e7a104b825c789366e0546a00f
jjcua <jjgamer371@gmail.com>
2013-12-11 15:49:59 +0800

    Model COmmit

--- SODAMvcApplication/SODAPortalMvcApplication/ViewModel/CustomerModel.cs
+++ SODAMvcApplication/SODAPortalMvcApplication/ViewModel/CustomerModel.cs
@@ -12,6 +12,10 @@ namespace SODAPortalMvcApplication.ViewModel
         public PortalServiceReference.SalesCode salesCode { get; set; }
         public PortalServiceReference.SalesPerson salesPerson { get; set; }
         public PortalServiceReference.Price price { get; set; }
+
+        public SODAPayPalSerRef.PayPalTrans paypal { get; set; }
+
+        public PortalServiceReference.Region rejoin { get; set; }
     }
 
     public class VerifyModel
@@ -19,5 +23,37 @@ namespace SODAPortalMvcApplication.ViewModel
         public PortalServiceReference.SalesPerson saleperson { get; set; }
         public PortalServiceReference.SalesCode salescode { get; set; }
         public PortalServiceReference.Price price { get; set; }
+        public PortalServiceReference.Region region { get; set; }
+        public decimal discountedPrice_A { get; set; }
+        public decimal discountedPrice_B { get; set; }
+        public decimal discountedPrice_C { get; set; }
+
+        public int selectedSubscription { get; set; }
+        public int qty { get; set; }
+
+        public enum Currency
+        {
+            USD,
+            EUR,
+            AUD,
+            GBP
+        };
+      
+        public static System.Globalization.CultureInfo getCultureInfo(Currency curenncy)
+        {
+            switch (curenncy)
+            {
+
+
+                case Currency.GBP: return new System.Globalization.CultureInfo("en-GB");    
+                case Currency.EUR:
+                    return new System.Globalization.CultureInfo("de-DE");
+                case Currency.AUD:
+                    return new System.Globalization.CultureInfo("en-AU");
+                default://USD default
+                    return new System.Globalization.CultureInfo("en-US");
+            }
+        }
     }
+  
 }
