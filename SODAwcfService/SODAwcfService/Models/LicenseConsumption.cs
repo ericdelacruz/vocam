a1662da1c25b21c3c0f0d253fb176315e991c18d
jjcua <jjgamer371@gmail.com>
2013-12-13 19:45:59 +0800

    Model COmmit

---
+++ SODAwcfService/SODAwcfService/Models/LicenseConsumption.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SODAwcfService.Models
+{
+    public class LicenseConsumption
+    {
+        public int Id { get; set; }
+
+        public int Consumed { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+
+        public long UserId { get; set; }
+    }
+}
