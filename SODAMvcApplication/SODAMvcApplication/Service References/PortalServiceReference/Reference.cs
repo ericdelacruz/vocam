328bba7b4664845c57e879fdc69d66c6c5c9980a
jjcua <jjgamer371@gmail.com>
2013-12-13 19:19:11 +0800

    Reference commit

--- SODAMvcApplication/SODAMvcApplication/Service References/PortalServiceReference/Reference.cs
+++ SODAMvcApplication/SODAMvcApplication/Service References/PortalServiceReference/Reference.cs
@@ -29,7 +29,19 @@ namespace SODAMvcApplication.PortalServiceReference {
         private System.Nullable<System.DateTime> DateSubscriptionEndField;
         
         [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private System.Nullable<System.DateTime> DateUpdatedField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
         private long IdField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private int LicensesField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private System.Nullable<long> PPIdField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private short RecurringTypeField;
         
         [System.Runtime.Serialization.OptionalFieldAttribute()]
         private long SalesCodeIdField;
@@ -74,6 +86,19 @@ namespace SODAMvcApplication.PortalServiceReference {
         }
         
         [System.Runtime.Serialization.DataMemberAttribute()]
+        public System.Nullable<System.DateTime> DateUpdated {
+            get {
+                return this.DateUpdatedField;
+            }
+            set {
+                if ((this.DateUpdatedField.Equals(value) != true)) {
+                    this.DateUpdatedField = value;
+                    this.RaisePropertyChanged("DateUpdated");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
         public long Id {
             get {
                 return this.IdField;
@@ -82,6 +107,45 @@ namespace SODAMvcApplication.PortalServiceReference {
                 if ((this.IdField.Equals(value) != true)) {
                     this.IdField = value;
                     this.RaisePropertyChanged("Id");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public int Licenses {
+            get {
+                return this.LicensesField;
+            }
+            set {
+                if ((this.LicensesField.Equals(value) != true)) {
+                    this.LicensesField = value;
+                    this.RaisePropertyChanged("Licenses");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public System.Nullable<long> PPId {
+            get {
+                return this.PPIdField;
+            }
+            set {
+                if ((this.PPIdField.Equals(value) != true)) {
+                    this.PPIdField = value;
+                    this.RaisePropertyChanged("PPId");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public short RecurringType {
+            get {
+                return this.RecurringTypeField;
+            }
+            set {
+                if ((this.RecurringTypeField.Equals(value) != true)) {
+                    this.RecurringTypeField = value;
+                    this.RaisePropertyChanged("RecurringType");
                 }
             }
         }
@@ -362,7 +426,13 @@ namespace SODAMvcApplication.PortalServiceReference {
         private decimal PriceAmtField;
         
         [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private decimal PriceAmt_BField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
         private int RegionIdField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private decimal priceAmt_CField;
         
         [global::System.ComponentModel.BrowsableAttribute(false)]
         public System.Runtime.Serialization.ExtensionDataObject ExtensionData {
@@ -427,6 +497,19 @@ namespace SODAMvcApplication.PortalServiceReference {
         }
         
         [System.Runtime.Serialization.DataMemberAttribute()]
+        public decimal PriceAmt_B {
+            get {
+                return this.PriceAmt_BField;
+            }
+            set {
+                if ((this.PriceAmt_BField.Equals(value) != true)) {
+                    this.PriceAmt_BField = value;
+                    this.RaisePropertyChanged("PriceAmt_B");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
         public int RegionId {
             get {
                 return this.RegionIdField;
@@ -435,6 +518,19 @@ namespace SODAMvcApplication.PortalServiceReference {
                 if ((this.RegionIdField.Equals(value) != true)) {
                     this.RegionIdField = value;
                     this.RaisePropertyChanged("RegionId");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public decimal priceAmt_C {
+            get {
+                return this.priceAmt_CField;
+            }
+            set {
+                if ((this.priceAmt_CField.Equals(value) != true)) {
+                    this.priceAmt_CField = value;
+                    this.RaisePropertyChanged("priceAmt_C");
                 }
             }
         }
@@ -459,6 +555,9 @@ namespace SODAMvcApplication.PortalServiceReference {
         private System.Runtime.Serialization.ExtensionDataObject extensionDataField;
         
         [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private string CurrencyField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
         private int IdField;
         
         [System.Runtime.Serialization.OptionalFieldAttribute()]
@@ -471,6 +570,19 @@ namespace SODAMvcApplication.PortalServiceReference {
             }
             set {
                 this.extensionDataField = value;
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public string Currency {
+            get {
+                return this.CurrencyField;
+            }
+            set {
+                if ((object.ReferenceEquals(this.CurrencyField, value) != true)) {
+                    this.CurrencyField = value;
+                    this.RaisePropertyChanged("Currency");
+                }
             }
         }
         
@@ -496,6 +608,99 @@ namespace SODAMvcApplication.PortalServiceReference {
                 if ((object.ReferenceEquals(this.RegionNameField, value) != true)) {
                     this.RegionNameField = value;
                     this.RaisePropertyChanged("RegionName");
+                }
+            }
+        }
+        
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        
+        protected void RaisePropertyChanged(string propertyName) {
+            System.ComponentModel.PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if ((propertyChanged != null)) {
+                propertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+    
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
+    [System.Runtime.Serialization.DataContractAttribute(Name="LicenseConsumption", Namespace="http://schemas.datacontract.org/2004/07/SODAwcfService.Models")]
+    [System.SerializableAttribute()]
+    public partial class LicenseConsumption : object, System.Runtime.Serialization.IExtensibleDataObject, System.ComponentModel.INotifyPropertyChanged {
+        
+        [System.NonSerializedAttribute()]
+        private System.Runtime.Serialization.ExtensionDataObject extensionDataField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private int ConsumedField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private System.DateTime DateUpdatedField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private int IdField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private long UserIdField;
+        
+        [global::System.ComponentModel.BrowsableAttribute(false)]
+        public System.Runtime.Serialization.ExtensionDataObject ExtensionData {
+            get {
+                return this.extensionDataField;
+            }
+            set {
+                this.extensionDataField = value;
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public int Consumed {
+            get {
+                return this.ConsumedField;
+            }
+            set {
+                if ((this.ConsumedField.Equals(value) != true)) {
+                    this.ConsumedField = value;
+                    this.RaisePropertyChanged("Consumed");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public System.DateTime DateUpdated {
+            get {
+                return this.DateUpdatedField;
+            }
+            set {
+                if ((this.DateUpdatedField.Equals(value) != true)) {
+                    this.DateUpdatedField = value;
+                    this.RaisePropertyChanged("DateUpdated");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public int Id {
+            get {
+                return this.IdField;
+            }
+            set {
+                if ((this.IdField.Equals(value) != true)) {
+                    this.IdField = value;
+                    this.RaisePropertyChanged("Id");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public long UserId {
+            get {
+                return this.UserIdField;
+            }
+            set {
+                if ((this.UserIdField.Equals(value) != true)) {
+                    this.UserIdField = value;
+                    this.RaisePropertyChanged("UserId");
                 }
             }
         }
@@ -579,6 +784,18 @@ namespace SODAMvcApplication.PortalServiceReference {
         
         [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPortalService/deleteRegion", ReplyAction="http://tempuri.org/IPortalService/deleteRegionResponse")]
         int deleteRegion(int id);
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPortalService/getLicenseConsumption", ReplyAction="http://tempuri.org/IPortalService/getLicenseConsumptionResponse")]
+        SODAMvcApplication.PortalServiceReference.LicenseConsumption[] getLicenseConsumption();
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPortalService/addLicenseConsumption", ReplyAction="http://tempuri.org/IPortalService/addLicenseConsumptionResponse")]
+        int addLicenseConsumption(SODAMvcApplication.PortalServiceReference.LicenseConsumption license);
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPortalService/updateLicenseConsumption", ReplyAction="http://tempuri.org/IPortalService/updateLicenseConsumptionResponse")]
+        int updateLicenseConsumption(SODAMvcApplication.PortalServiceReference.LicenseConsumption license);
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPortalService/delteLicenseConsumption", ReplyAction="http://tempuri.org/IPortalService/delteLicenseConsumptionResponse")]
+        int delteLicenseConsumption(int id);
     }
     
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
@@ -694,6 +911,22 @@ namespace SODAMvcApplication.PortalServiceReference {
         
         public int deleteRegion(int id) {
             return base.Channel.deleteRegion(id);
+        }
+        
+        public SODAMvcApplication.PortalServiceReference.LicenseConsumption[] getLicenseConsumption() {
+            return base.Channel.getLicenseConsumption();
+        }
+        
+        public int addLicenseConsumption(SODAMvcApplication.PortalServiceReference.LicenseConsumption license) {
+            return base.Channel.addLicenseConsumption(license);
+        }
+        
+        public int updateLicenseConsumption(SODAMvcApplication.PortalServiceReference.LicenseConsumption license) {
+            return base.Channel.updateLicenseConsumption(license);
+        }
+        
+        public int delteLicenseConsumption(int id) {
+            return base.Channel.delteLicenseConsumption(id);
         }
     }
 }
