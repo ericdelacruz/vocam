31308bbb8e38be1b3d7c73bba94acdd338f05e8a
jjcua <jjgamer371@gmail.com>
2013-12-23 13:52:38 +0800

    model commit

---
+++ SODAwcfService/SODAwcfService/Models/FreePPT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SODAwcfService.Models
+{
+    public class FreePPT
+    {
+        public string FileName { get; set; }
+
+        public string PPTType { get; set; }
+
+        public int RegionId { get; set; }
+
+        public int Id { get; set; }
+
+        public string DisplayText { get; set; }
+    }
+}
