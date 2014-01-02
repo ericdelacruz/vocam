658dd3e6ef46d9137b8ea6e98d72f27dcb94bed0
jjcua <jjgamer371@gmail.com>
2014-01-02 15:09:28 +0800

    Model Commit

--- SODAMvcApplication/SODAPortalMvcApplication/ViewModel/UserModel.cs
+++ SODAMvcApplication/SODAPortalMvcApplication/ViewModel/UserModel.cs
@@ -30,4 +30,16 @@ namespace SODAPortalMvcApplication.ViewModel
 
         public string CompanyUrl { get; set; }
     }
+
+    public class changePasswordModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("Password", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
 }
