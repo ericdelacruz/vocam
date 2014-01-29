738df9c3b451db888ab0b1f8edf7b856dc705f38
jjcua <jjgamer371@gmail.com>
2014-01-29 08:04:32 +0800

    Sales role

--- SODAMvcApplication/SODAPortalMvcApplication/Controllers/salesController.cs
+++ SODAMvcApplication/SODAPortalMvcApplication/Controllers/salesController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using PagedList;
 namespace SODAPortalMvcApplication.Controllers
 {
     public class salesController : Controller
@@ -12,23 +12,48 @@ namespace SODAPortalMvcApplication.Controllers
         // GET: /sales/
         private AccountServiceRef.AccountServiceClient account = new AccountServiceRef.AccountServiceClient();
         private PortalServiceReference.PortalServiceClient portalClient = new PortalServiceReference.PortalServiceClient();
-        public ActionResult Index()
+        public ActionResult Index(string df, string dt, string sc, int? page)
         {
             if (!isUserSessionActive())
             {
-                return RedirectToAction("index", "Home");
+                return RedirectToAction("login", "Home");
             }
             else
             {
 
-                var reportlist = from customer in portalClient.getCustomer()
-                                 join accnt in account.getAccount(Session["Username"].ToString()) on customer.Id equals accnt.Id
-                                 join sc in portalClient.getSaleCode() on customer.SalesCodeId equals sc.Id
-                                 join sp in portalClient.getSalePerson() on customer.SalesCodeId equals sp.SalesCodeId
-                                 where sp.UserId == accnt.Id                           
-                                 select new ViewModel.ReportViewModel() { account = accnt, customer = customer, salesCode = sc };
-                
-                return View(reportlist);
+                var reportlist = from customer in portalClient.getCustomer()  
+                                 join salescode in portalClient.getSaleCode() on customer.SalesCodeId equals salescode.Id
+                                 join salesperson in portalClient.getSalePerson() on customer.SalesCodeId equals salesperson.SalesCodeId
+                                 join accnt in account.getAccount(Session["Username"].ToString()) on salesperson.UserId equals accnt.Id 
+                                 orderby customer.DatePurchase descending
+                                 select new ViewModel.ReportViewModel() { account = accnt, customer = customer, salesCode = salescode };
+
+                if (!string.IsNullOrEmpty(df) && !string.IsNullOrEmpty(dt))
+                {
+                    reportlist = from report in reportlist
+                                 where report.customer.DatePurchase >= DateTime.Parse(df) && report.customer.DatePurchase <= DateTime.Parse(dt)
+                                 select report;
+
+                }
+                else if (!string.IsNullOrEmpty(df))
+                {
+                    reportlist = from report in reportlist
+                                 where report.customer.DatePurchase == DateTime.Parse(df)
+                                 select report;
+                }
+
+                if (!string.IsNullOrEmpty(sc))
+                {
+                    reportlist = from report in reportlist
+                                 where report.salesCode.Sales_Code.ToLower() == sc.ToLower().Trim()
+                                 select report;
+                }
+                int pageSize = 10;
+                int pageNumber = page ?? 1;
+                ViewBag.dt = dt ?? "";
+                ViewBag.df = df ?? "";
+                ViewBag.sc = sc ?? "";
+                return View(reportlist.ToList().ToPagedList(pageNumber, pageSize));
             }
             
         }
@@ -53,7 +78,7 @@ namespace SODAPortalMvcApplication.Controllers
                                  join accnt in account.getAccount(Session["Username"].ToString()) on customer.Id equals accnt.Id
                                  join sc in portalClient.getSaleCode() on customer.SalesCodeId equals sc.Id
                                  join sp in portalClient.getSalePerson() on customer.SalesCodeId equals sp.SalesCodeId
-                                 where customer.DatePurchase.Value.DayOfYear >= dtstart.DayOfYear && customer.DatePurchase.Value.DayOfYear < dtend.DayOfYear //&& sp.UserId == accnt.Id
+                                 where customer.DatePurchase.Value.DayOfYear >= dtstart.DayOfYear && customer.DatePurchase.Value.DayOfYear < dtend.DayOfYear 
                                  select new ViewModel.ReportViewModel() { account = accnt, customer = customer,salesCode = sc };
                 return View(reportlist);
             }
